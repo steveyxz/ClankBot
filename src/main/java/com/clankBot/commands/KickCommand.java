6a6fd95dc7c2c77687d6414530a114ca2675cf60
steveyxz <65844236+steveyxz@users.noreply.github.com>
2021-04-23 19:32:15 +1000

    Added kick reason (Minor fix)

--- src/main/java/com/clankBot/commands/KickCommand.java
+++ src/main/java/com/clankBot/commands/KickCommand.java
@@ -1,6 +1,5 @@
 package com.clankBot.commands;
 
-import com.clankBot.Main;
 import com.clankBot.enums.util.Category;
 import com.clankBot.util.EmbedCreator;
 import com.clankBot.util.GlobalMethods;
@@ -11,9 +10,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.exceptions.HierarchyException;
 import net.dv8tion.jda.internal.utils.PermissionUtil;
 
-import java.lang.reflect.Member;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Objects;
 
 public class KickCommand extends Command {
@@ -39,18 +36,37 @@ public class KickCommand extends Command {
             return;
         }
 
+        StringBuilder reason = new StringBuilder();
+        if (args.length > 1) {
+            for (int i = 0; i < args.length; i++) {
+                if (i == 0) {
+                    continue;
+                }
+                reason.append(args[i]);
+                reason.append(" ");
+            }
+        }
+
         try {
             User user = User.fromId(args[0].substring(3, args[0].length() - 1));
 
             if (PermissionUtil.canInteract(Objects.requireNonNull(e.getMember()),
                     Objects.requireNonNull(e.getGuild().getMember(user)))) {
-                e.getGuild().kick(Objects.requireNonNull(e.getGuild().getMember(user))).queue();
+                if (args.length < 2) {
+                    e.getGuild().kick(Objects.requireNonNull(e.getGuild().getMember(user))).queue();
+                } else {
+                    e.getGuild().kick(Objects.requireNonNull(e.getGuild().getMember(user)), new String(reason)).queue();
+                }
             } else {
                 e.getChannel().sendMessage(EmbedCreator.createErrorEmbed(embedBuilder, "You cannot kick this member because (s)he is of higher authority than you").build()).queue();
                 return;
             }
 
-            e.getChannel().sendMessage(EmbedCreator.createSuccessEmbed(embedBuilder, "SUCCESSFUL KICK", "Kick member " + args[0]).build()).queue();
+            if (args.length < 2) {
+                e.getChannel().sendMessage(EmbedCreator.createSuccessEmbed(embedBuilder, "SUCCESSFUL KICK", "Kick member " + args[0]).build()).queue();
+            } else {
+                e.getChannel().sendMessage(EmbedCreator.createSuccessEmbed(embedBuilder, "SUCCESSFUL KICK", "Kick member " + args[0] + " for `" + new String(reason)).build() + "`").queue();
+            }
 
         } catch (HierarchyException error) {
             e.getChannel().sendMessage(EmbedCreator.createErrorEmbed(embedBuilder, "Bot cannot kick this member: INSUFFICIENT PERMISSIONS").build()).queue();
